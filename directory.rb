f068dfb2f46af214d4380c8ac100755bc99f3f81
adam.beresford <adam56.beresford@gmail.com>
2019-09-18 15:47:38 +0100

    curly brackets fix

--- directory.rb
+++ directory.rb
@@ -1,17 +1,17 @@
 
 #assigning students array with all students names
 students = [
-  [name: "Dr. Hannibal Lecter", cohort: :november],
-  [name: "Darth Vader", cohort: :november],
-  [name: "Nurse Ratched", cohort: :november],
-  [name: "Michael Corleone", cohort: :november],
-  [name: "Alex DeLarge", cohort: :november],
-  [name: "The Wicked Witch of the West", cohort: :november],
-  [name: "Terminator", cohort: :november],
-  [name: "Freddy Krueger", cohort: :november],
-  [name: "The Joker", cohort: :november],
-  [name: "Joffrey Baratheon", cohort: :november],
-  [name: "Norman Bates", cohort: :november]
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
 ]
 # finally, we print the total number of students
 
