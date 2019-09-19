cd9c7402de58bad90fe75a48ba65ba8aca84ae77
2Blagden <benblagden0@gmail.com>
2019-09-19 15:15:15 +0100

    centered the text

--- directory.rb
+++ directory.rb
@@ -21,20 +21,20 @@ def input_students
 end
 
 def print_header()
-  puts "The students of Villans Academy"
-  puts "__________________"
+  puts "The students of Villans Academy".center(100)
+  puts "__________________".center(100)
 end
 
 def print(students)
   students.each do |student|
     if student[:name][0].include?('a') && student[:name].length < 13
-      puts "#{student[:name]} (#{student[:cohort]} cohort) #{student[:sport]} Sport #{student[:country]}"
+      puts "#{student[:name]} (#{student[:cohort]} cohort) #{student[:sport]} Sport #{student[:country]}".center(100)
     end
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great studentss"
+  puts "Overall, we have #{students.count} great studentss".center(100)
 end
 
 students = input_students
